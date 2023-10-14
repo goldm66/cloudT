3fc48cc5e5c54dac447beb63923d5966e85d3bd3
goldm66 <117630364+goldm66@users.noreply.github.com>
2023-10-14 09:04:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 let userID = 'a321f7f4-c556-41f7-b958-ed6c07ac3e63';
 
 //const proxyIPs = ['time.is'];'104.17.253.207','45.66.128.240'
-const proxyIPs = ['45.66.128.240','45.195.62.11']
+const proxyIPs = ['45.195.62.57','43.154.209']
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
